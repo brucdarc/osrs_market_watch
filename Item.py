c2f9abf2bfb0d4ffbd67b6731b861da1093c64d6
brucdarc <cutreaper@gmail.com>
2020-08-31 23:36:30 -0600

    refactored files, python file for each class

---
+++ Item.py
@@ -0,0 +1,160 @@
+
+
+import time
+import numpy
+
+
+class Item:
+
+
+    def __init__(self, id, name, members, sp, buy_average, buy_quantity, sell_average, sell_quantity, overall_average, overall_quantity):
+        #constructor
+        #should only be called in setup, or when updates add new items to the game.
+        #sets some variables, and sets up lists for variables that will be taken every time period for market history data
+        self.id = id
+        self.name = name
+        self.members = members
+        self.sp = sp
+        self.buy_averages = [buy_average]
+        self.buy_quantities = [buy_quantity]
+        self.sell_averages = [sell_average]
+        self.sell_quantities = [sell_quantity]
+        self.overall_averages = [overall_average]
+        self.overall_quantities = [overall_quantity]
+        self.timestamps = [time.time()]
+        if sell_average != 0 and buy_average !=0:
+            self.margins = [buy_average - sell_average]
+            self.rois = [(buy_average - sell_average) / sell_average]
+        else:
+            self.margins = [0]
+            self.rois = [0]
+
+    def update(self, buy_average, buy_quantity, sell_average, sell_quantity, overall_average, overall_quantity):
+        #called to add another history point for the market data of an item
+        #appends price and volume data to lists, with a timestamp
+        self.buy_averages.append(buy_average)
+        self.buy_quantities.append(buy_quantity)
+        self.sell_averages.append(sell_average)
+        self.sell_quantities.append(sell_quantity)
+        self.overall_averages.append(overall_average)
+        self.overall_quantities.append(overall_quantity)
+        self.timestamps.append(time.time())
+        if sell_average != 0 and buy_average !=0:
+            self.margins.append(buy_average - sell_average)
+            self.rois.append((buy_average - sell_average) / sell_average)
+        else:
+            self.margins.append(0)
+            self.rois.append(0)
+
+
+    def cmargin(self):
+        return self.margins[-1]
+    def cbuy_quant(self):
+        return self.buy_quantities[-1]
+    def cbuy_price(self):
+        return self.buy_averages[-1]
+    def csell_quant(self):
+        return self.sell_quantities[-1]
+    def csell_price(self):
+        return self.overall_averages[-1]
+    def croi(self):
+        return self.rois[-1]
+
+    def getAverageMargin(self, time_amount):
+        return self.getAverageBuyPrice(time_amount) - self.getAverageSellPrice(time_amount)
+
+    def getAverageRoi(self, time_amount):
+        sell_av =  self.getAverageSellPrice(time_amount)
+        buy_av = self.getAverageBuyPrice(time_amount)
+        if sell_av == 0 or buy_av == 0:
+            return 0
+        return (buy_av - sell_av) / sell_av
+
+    def getAverageBuyPrice(self, time_amount):
+        #Todo make this take percentile not just average, bad for flips
+        time_cutoff = time.time() - time_amount
+        split_index = 0
+
+        while(self.timestamps[split_index] < time_cutoff):
+            split_index += 1
+
+
+        buy_prices = self.buy_averages[split_index:]
+
+        running_average = 0
+        non_datafull_timestamps = 0
+        meaningful_datapoints = []
+
+        for price in buy_prices:
+            running_average += price
+            if price == 0: non_datafull_timestamps += 1
+            else: meaningful_datapoints.append(price)
+
+
+        if(non_datafull_timestamps != len(buy_prices)):
+            running_average /= (len(buy_prices)-non_datafull_timestamps)
+
+        percentile = 90
+        result = 0
+        if len(meaningful_datapoints) != 0:
+            result = numpy.percentile(meaningful_datapoints, percentile)
+
+        return result
+
+
+
+    def getAverageSellPrice(self, time_amount):
+        #Todo make this take percentile not just average, bad for flips
+        time_cutoff = time.time() - time_amount
+        split_index = 0
+
+        while(self.timestamps[split_index] < time_cutoff):
+            split_index += 1
+
+
+        sell_prices = self.sell_averages[split_index:]
+
+        running_average = 0
+        meaningful_datapoints = []
+        non_datafull_timestamps = 0
+
+        for price in sell_prices:
+            running_average += price
+            if price == 0: non_datafull_timestamps += 1
+            else: meaningful_datapoints.append(price)
+
+        if(non_datafull_timestamps != len(sell_prices)):
+            running_average /= (len(sell_prices)-non_datafull_timestamps)
+
+        percentile = 10
+        result = 0
+        if len(meaningful_datapoints) != 0:
+            result = numpy.percentile(meaningful_datapoints, percentile)
+
+        return result
+
+
+    def last_10_buy_quant(self):
+        running_sum = 0
+        index = 0
+        while index > -10:
+            index -= 1
+            running_sum += self.buy_quantities[index]
+        return running_sum
+
+    def last_10_sell_quant(self):
+        running_sum = 0
+        index = 0
+        while index > -10:
+            index -= 1
+            running_sum += self.buy_quantities[index]
+        return running_sum
+
+    def __str__(self):
+        temp = 'lmao'#ayy lmao
+        CurrentROIitems_str = '{: <8} {: <28} most recent margin: {: <7} most recent ROI: {: <17} buy qty: {: <8} sell qty: {: <8} buy avg: {: <8} sell avg: {: <8}\n         Recent activity:\t      bought:{: <20} sold:{: <10}\nOver the last 24 hours:               average margin: {: <11} average ROI: {: <21} average buy price: {: <17} average sell price: {}\n'
+        CurrentROIitems_list = [self.overall_quantities, self.name, self.margins, self.rois, self.buy_quantities, self.sp, self.sell_quantities, self.buy_averages, self.sell_averages]
+        for row in CurrentROIitems_list:
+            return (CurrentROIitems_str.format(self.overall_quantities[-1], self.name, self.margins[-1], str(round(self.rois[-1], 3)), self.buy_quantities[-1], self.sell_quantities[-1], self.buy_averages[-1], self.sell_averages[-1], self.last_10_buy_quant(), self.last_10_sell_quant(), str(round(self.getAverageMargin(86400), 3)), str(round(self.getAverageRoi(86400), 3)), str(round(self.getAverageBuyPrice(86400), 3)), str(round(self.getAverageSellPrice(86400), 3))).format(*row))
+
+
--- osrs_market.py
+++ osrs_market.py
@@ -2,255 +2,9 @@ import requests
 import json
 import pickle
 import time
-
-class Item:
-
-
-    def __init__(self, id, name, members, sp, buy_average, buy_quantity, sell_average, sell_quantity, overall_average, overall_quantity):
-        #constructor
-        #should only be called in setup, or when updates add new items to the game.
-        #sets some variables, and sets up lists for variables that will be taken every time period for market history data
-        self.id = id
-        self.name = name
-        self.members = members
-        self.sp = sp
-        self.buy_averages = [buy_average]
-        self.buy_quantities = [buy_quantity]
-        self.sell_averages = [sell_average]
-        self.sell_quantities = [sell_quantity]
-        self.overall_averages = [overall_average]
-        self.overall_quantities = [overall_quantity]
-        self.timestamps = [time.time()]
-        if sell_average != 0 and buy_average !=0:
-            self.margins = [buy_average - sell_average]
-            self.rois = [(buy_average - sell_average) / sell_average]
-        else:
-            self.margins = [0]
-            self.rois = [0]
-
-    def update(self, buy_average, buy_quantity, sell_average, sell_quantity, overall_average, overall_quantity):
-        #called to add another history point for the market data of an item
-        #appends price and volume data to lists, with a timestamp
-        self.buy_averages.append(buy_average)
-        self.buy_quantities.append(buy_quantity)
-        self.sell_averages.append(sell_average)
-        self.sell_quantities.append(sell_quantity)
-        self.overall_averages.append(overall_average)
-        self.overall_quantities.append(overall_quantity)
-        self.timestamps.append(time.time())
-        if sell_average != 0 and buy_average !=0:
-            self.margins.append(buy_average - sell_average)
-            self.rois.append((buy_average - sell_average) / sell_average)
-        else:
-            self.margins.append(0)
-            self.rois.append(0)
-
-
-    def cmargin(self):
-        return self.margins[-1]
-    def cbuy_quant(self):
-        return self.buy_quantities[-1]
-    def cbuy_price(self):
-        return self.buy_averages[-1]
-    def csell_quant(self):
-        return self.sell_quantities[-1]
-    def csell_price(self):
-        return self.overall_averages[-1]
-    def croi(self):
-        return self.rois[-1]
-
-    def getAverageMargin(self, time_amount):
-        return self.getAverageBuyPrice(time_amount) - self.getAverageSellPrice(time_amount)
-
-    def getAverageRoi(self, time_amount):
-        sell_av =  self.getAverageSellPrice(time_amount)
-        buy_av = self.getAverageBuyPrice(time_amount)
-        if sell_av == 0 or buy_av == 0:
-            return 0
-        return (buy_av - sell_av) / sell_av
-
-    def getAverageBuyPrice(self, time_amount):
-        time_cutoff = time.time() - time_amount
-        split_index = 0
-
-        while(self.timestamps[split_index] < time_cutoff):
-            split_index += 1
-
-
-        buy_prices = self.buy_averages[split_index:]
-
-        running_average = 0
-        non_datafull_timestamps = 0
-
-        for price in buy_prices:
-            running_average += price
-            if price == 0: non_datafull_timestamps += 1
-
-        if(non_datafull_timestamps != len(buy_prices)):
-            running_average /= (len(buy_prices)-non_datafull_timestamps)
-
-        return running_average
-
-    def getAverageSellPrice(self, time_amount):
-        time_cutoff = time.time() - time_amount
-        split_index = 0
-
-        while(self.timestamps[split_index] < time_cutoff):
-            split_index += 1
-
-
-        sell_prices = self.sell_averages[split_index:]
-
-        running_average = 0
-        non_datafull_timestamps = 0
-
-        for price in sell_prices:
-            running_average += price
-            if price == 0: non_datafull_timestamps += 1
-
-        if(non_datafull_timestamps != len(sell_prices)):
-            running_average /= (len(sell_prices)-non_datafull_timestamps)
-
-        return running_average
-
-
-    def last_10_buy_quant(self):
-        running_sum = 0
-        index = 0
-        while index > -10:
-            index -= 1
-            running_sum += self.buy_quantities[index]
-        return running_sum
-
-    def last_10_sell_quant(self):
-        running_sum = 0
-        index = 0
-        while index > -10:
-            index -= 1
-            running_sum += self.buy_quantities[index]
-        return running_sum
-
-    def __str__(self):
-        temp = 'lmao'#ayy lmao
-        CurrentROIitems_str = '{: <8} {: <28} most recent margin: {: <7} most recent ROI: {: <17} buy qty: {: <8} sell qty: {: <8} buy avg: {: <8} sell avg: {: <8}\n         Recent activity:\t      bought:{: <20} sold:{: <10}\nOver the last 24 hours:               average margin: {: <11} average ROI: {: <21} average buy price: {: <17} average sell price: {}\n'
-        CurrentROIitems_list = [self.overall_quantities, self.name, self.margins, self.rois, self.buy_quantities, self.sp, self.sell_quantities, self.buy_averages, self.sell_averages]
-        for row in CurrentROIitems_list:
-            return (CurrentROIitems_str.format(self.overall_quantities[-1], self.name, self.margins[-1], str(round(self.rois[-1], 3)), self.buy_quantities[-1], self.sell_quantities[-1], self.buy_averages[-1], self.sell_averages[-1], self.last_10_buy_quant(), self.last_10_sell_quant(), str(round(self.getAverageMargin(86400), 3)), str(round(self.getAverageRoi(86400), 3)), str(round(self.getAverageBuyPrice(86400), 3)), str(round(self.getAverageSellPrice(86400), 3))).format(*row))
-
-
-
-
-
-
-
-class MarketDatabase:
-    def __init__(self, filename):
-        #reads a preset database file and converts it into objects
-        filehandler = open(filename, 'rb')
-        self.filename = filename
-        try:
-            self.itemList = pickle.load(filehandler)
-        except EOFError:
-            self.itemList = []
-
-    def writeToFile(self, filename):
-        #writes a database file in pickle object notation
-        filehandler = open(filename, 'wb')
-        pickle.dump(self.itemList, filehandler)
-
-    def updateItem(self, id, name, members, sp, buy_average, buy_quantity, sell_average, sell_quantity, overall_average, overall_quantity):
-        #Updated
-        #added item buy price, sell price, ect. to lists that keep track of them over time
-        #added some sort of timestamping functionality
-
-        #if item is in itemlist, append data to lists
-        found = False
-        for item in self.itemList:
-            if item.id == id:
-                item.update(buy_average, buy_quantity, sell_average, sell_quantity, overall_average, overall_quantity)
-                found = True
-
-        #else if item is not in itemlist, add it to itemlist and initialize it
-        if not found:
-            item = Item(id, name, members, sp, buy_average, buy_quantity, sell_average, sell_quantity, overall_average, overall_quantity)
-            self.itemList.append(item)
-
-        return "lmao"
-
-    def getHighestMargins(self, listLen):
-        assert(len(self.itemList) > listLen)
-        topn = []
-        for x in range(0,listLen):
-            currentTop = None
-            for item in self.itemList:
-                if currentTop is None:
-                    if item not in topn:
-                        currentTop = item
-                else:
-                    if item.cmargin() > currentTop.cmargin() and item not in topn:
-                        currentTop = item
-            topn.append(currentTop)
-        return topn
-
-    def getHighestRois(self, listLen):
-        assert(len(self.itemList) > listLen)
-        topn = []
-        for x in range(0,listLen):
-            currentTop = None
-            for item in self.itemList:
-                if item.cbuy_price() > 50:
-                    if currentTop is None:
-                        if item not in topn:
-                            currentTop = item
-                    else:
-                        if item.croi() > currentTop.croi() and item not in topn:
-                            currentTop = item
-            topn.append(currentTop)
-        return topn
-
-    def getHighestMargins_with_time(self, howmany, time_ago):
-        assert (len(self.itemList) > howmany)
-        topn = []
-        for x in range(0, howmany):
-            currentTop = None
-            for item in self.itemList:
-                if item.cbuy_price() > 50:
-                    if currentTop is None:
-                        if item not in topn:
-                            currentTop = item
-                    else:
-                        if item.getAverageMargin(time_ago) > currentTop.getAverageMargin(time_ago) and item not in topn:
-                            currentTop = item
-            topn.append(currentTop)
-        return topn
-
-    def getHighestRois_with_time(self, howmany, time_ago):
-        assert (len(self.itemList) > howmany)
-        topn = []
-        for x in range(0, howmany):
-            currentTop = None
-            for item in self.itemList:
-                if item.cbuy_price() > 50:
-                    if currentTop is None:
-                        if item not in topn:
-                            currentTop = item
-                    else:
-                        if item.getAverageRoi(time_ago) > currentTop.getAverageRoi(time_ago) and item not in topn:
-                            currentTop = item
-            topn.append(currentTop)
-        return topn
-
-
-
-
-
-    def __str__(self):
-        ret = ""
-        for item in self.itemList:
-            ret += str(item) + "\n"
-        return ret
-
-
+import numpy
+from Item import Item
+from MarketDatabase import MarketDatabase
 
 
 
@@ -266,14 +20,20 @@ for item_data in market_data:
 
 tiem = 86400
 
-highMarg = database.getHighestRois_with_time(100, tiem)
-
+#highMarg = database.getHighestRois_with_time(100, tiem, 5000)
+highMarg = []
 
 
 
 print("Items currently with the return on investment")
 for item in highMarg:
     print(item)
+
+print("\n\n\nspecific test items:\n\n\n")
+
+for item in database.itemList:
+    if 'book page set' in item.name:
+        print(item)
 
 #print(database)
 database.writeToFile("osrs_data")
