7e99657f7a07d1b000ca6107437bcd53dcdbd889
brucdarc <darkdark@DESKTOP-LNM8S60.localdomain>
2020-09-09 17:50:23 -0600

    react is killing my braincells

--- Market_Snapshot_Recorder.py
+++ Market_Snapshot_Recorder.py
@@ -9,14 +9,18 @@ from MarketDatabase import MarketDatabase
 snapshot_number = 0
 
 while(True):
-    market_data = requests.get('https://rsbuddy.com/exchange/summary.json')
-    market_data = json.loads(market_data.content.decode('utf8'))
-    database = MarketDatabase("osrs_data")
+    try:
+        market_data = requests.get('https://rsbuddy.com/exchange/summary.json')
+        market_data = json.loads(market_data.content.decode('utf8'))
+        database = MarketDatabase("osrs_data")
 
-    for item_data in market_data:
-        item = market_data[item_data]
-        database.updateItem(item['id'], item['name'], item['members'], item['sp'], int(item['buy_average']), item['buy_quantity'], int(item['sell_average']), item['sell_quantity'], item['overall_average'], item['overall_quantity'])
-    database.writeToFile("osrs_data")
-    snapshot_number += 1
-    print("done with market snapshot " + str(snapshot_number) + " at time " + str(time.time()))
+        for item_data in market_data:
+            item = market_data[item_data]
+            database.updateItem(item['id'], item['name'], item['members'], item['sp'], int(item['buy_average']), item['buy_quantity'], int(item['sell_average']), item['sell_quantity'], item['overall_average'], item['overall_quantity'])
+        database.writeToFile("osrs_data")
+        snapshot_number += 1
+        print("done with market snapshot " + str(snapshot_number) + " at time " + str(time.time()))
+    except Exception:
+        print("connection failed, trying again in 10 minutes")
+
     time.sleep(60*10)
--- market/src/App.js
+++ market/src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from 'react-bootstrap/Image'
+import { Container, Row, Grid } from 'react-bootstrap';
 
 function App() {
   return (
+    <div>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload derp derp derp.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Image src="https://oldschool.runescape.wiki/images/6/63/Coins_detail.png?404bc"/>
+        <form>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </header>
+    </div>
+    <div>
+        <Grid>
+          <Row>
+            <Col>1 of 2</Col>
+            <Col>2 of 2</Col>
+          </Row>
+          <Row>
+            <Col>1 of 3</Col>
+            <Col>2 of 3</Col>
+            <Col>3 of 3</Col>
+          </Row>
+        </Grid>
+    </div>
     </div>
   );
 }
