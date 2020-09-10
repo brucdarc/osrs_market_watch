02e36ae8da2d4b9b440a6a66d6b896bce0d6ddbf
brucdarc <darkdark@DESKTOP-LNM8S60.localdomain>
2020-09-10 15:47:26 -0600

    added start script to start database recorder and discord bot from same shell

--- market/src/App.js
+++ market/src/App.js
@@ -1,37 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import Image from 'react-bootstrap/Image'
-import { Container, Row, Grid } from 'react-bootstrap';
+import { Container, Row, Col, Jumbotron } from 'reactstrap';
+import Button from "@material-ui/core/Button";
+import Thing from './Thing';
 
 function App() {
   return (
-    <div>
     <div className="App">
-      <header className="App-header">
-        <Image src="https://oldschool.runescape.wiki/images/6/63/Coins_detail.png?404bc"/>
-        <form>
-          <label>
-            Name:
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </header>
-    </div>
-    <div>
-        <Grid>
-          <Row>
-            <Col>1 of 2</Col>
-            <Col>2 of 2</Col>
-          </Row>
-          <Row>
-            <Col>1 of 3</Col>
-            <Col>2 of 3</Col>
-            <Col>3 of 3</Col>
-          </Row>
-        </Grid>
-    </div>
+      <Thing />
     </div>
   );
 }
---
+++ start_script.sh
@@ -0,0 +1,2 @@
+python3 Market_Snapshot_Recorder.py &
+python3 discord_bot_osrs_market.py
